cfab03d8bdef1dee3383226357296afa2ff6ac06
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-07-04 19:25:19 -0300

    fake service added

--- AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -5,17 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private	readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
         {
             //	Get	to-do	items	from	database
+            var	items	=	await _todoItemService.GetIncompleteItemsAsync();
             //	Put	items	into	a	model
+            TodoViewModel viewModel =new TodoViewModel()
+            {
+                Items=items
+            };
             //	Render	view	using	the	model
-             return View();	
+             return View(viewModel);	
 
         }
     }
---
+++ AspNetCoreTodo/AspNetCoreTodo/Services/FakeTodoItemService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Aprender netcore",
+                DueAt = DateTimeOffset.Now.AddDays(90)
+            };            
+            
+            var item2 = new TodoItem
+            {
+                Title = "Aprender kitesurf",
+                DueAt = DateTimeOffset.Now.AddDays(90)
+            };
+
+            return Task.FromResult(new TodoItem[]{ item1, item2});
+        }
+    }
+}
