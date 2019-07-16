e4e960425a70e5d30c93ac6614d259130c0af641
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-07-16 11:26:35 -0300

    persistence and service for TodoItem added

--- AspNetCoreTodo/AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Startup.cs
@@ -46,6 +46,7 @@ namespace AspNetCoreTodo
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSingleton<ITodoItemService,	FakeTodoItemService>();
+            //services.AddScoped<ITodoItemService, TodoItemService>();
 
         }
 
